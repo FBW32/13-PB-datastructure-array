4b2011f0743771c318744c7bb30a3e3014c35b45
Abayomi <mrbayus2014@gmail.com>
2020-08-12 14:50:39 +0200

    datastructure-array

---
+++ bayo/datastructure-array.js
@@ -0,0 +1,21 @@
+// 12.08.2020
+// EXERCISE 1
+// Declare a variable named myArray and store an array that contains 5 elements. Print the array.
+let myArray = [
+    'London',
+    'Berlin',
+    'New York',
+    'Lagos',
+    'Delhi'
+];
+console.log(myArray);
+
+
+// EXERCISE 2
+// Access the 3rd element in the array and save that to another variable named thirdElement. Print the thirdElement.
+console.log(myArray[3]);
+
+// EXERCISE 3
+// Reassign the fourth element of the array and give it a value of "fourth element". Print the array once more.
+myArray[4] = "Tokyo";
+console.log(myArray);

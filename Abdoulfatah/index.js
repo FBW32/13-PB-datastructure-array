571c269d4a8dbdc8e9f6c1909b1ead11a4bc2e03
kartish-A <abd.kartish@gmail.com>
2020-08-12 13:41:52 +0200

    solution

---
+++ Abdoulfatah/index.js
@@ -0,0 +1,11 @@
+// first question:
+let myArray = ['element1', 'element2', 'element3', 'element4', 'element5'];
+console.log(myArray);
+
+// second question:
+myArray[2] = "thirdElement";
+console.log(myArray[2]);
+
+// third question:
+myArray[3] = "fourth element";
+console.log(myArray);

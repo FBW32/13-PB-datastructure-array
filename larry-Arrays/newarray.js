6bb9ae86eb8b489ce4a11382995f480b63cec346
LawrenzoDM <lawrenzodelmc@outlook.com>
2020-08-12 14:00:53 +0200

    adding arrays

---
+++ larry-Arrays/newarray.js
@@ -0,0 +1,15 @@
+//1.)
+let myArray = [];
+
+myArray = ["R.kelly","usher","boyz 11 men","P-Diddy","50 cent"]
+console.log(myArray);  // [ 'R.kelly', 'usher', 'boyz 11 men', 'P-Diddy', '50 cent' ]
+
+//2.)
+
+console.log(myArray[3]);  // P-Diddy
+
+let thirdElement = "P-Diddy";
+
+myArray[4] = "fourth element";
+
+console.log(myArray);  // [ 'R.kelly', 'usher', 'boyz 11 men', 'P-Diddy', 'fourth element' ]
